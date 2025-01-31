36f317d8a8f1754f71866e4b9b71fc5516350c68
Nastya Sorokova <ngrigis35@gmail.com>
2025-01-31 10:41:32 +0300

    Feat: connecting a slider in js, connecting a subscription form in js

--- src/main.js
+++ src/main.js
@@ -6,3 +6,20 @@ document.addEventListener("DOMContentLoaded", startCountdown);
 
 // homepage_slider
 
+
+//Slider
+
+import {updateSlider} from "./slider.js";
+updateSlider();
+
+import {validateEmail} from "./subscription.js";
+validateEmail();
+
+
+
+
+
+
+
+
+

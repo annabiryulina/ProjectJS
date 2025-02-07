1ae2775df503179974f04ba9a8d55ac10ec2ad12
Black8Jade <irinakhudina@gmail.com>
2025-02-08 02:42:16 +0300

    fixed the first part of the homepage

---
+++ src/main.js
@@ -0,0 +1,32 @@
+// homepage_timer
+
+import { startCountdown } from './homepage_timer.js';
+
+document.addEventListener("DOMContentLoaded", startCountdown);
+
+// homepage_slider
+
+import { initSlider } from './homepage_slider.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  initSlider();
+});
+
+
+//Slider
+
+import {updateSlider} from "./slider.js";
+updateSlider();
+
+import {validateEmail} from "./subscription.js";
+validateEmail();
+
+
+
+
+
+
+
+
+
+
---
+++ src/signup.js
@@ -0,0 +1,64 @@
+import { initializeApp } from "firebase/app";
+import { createUserWithEmailAndPassword, getAuth, signInWithPopup, GoogleAuthProvider} from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAytz-Il5aqBSvLfjKJp6bKjbbkDcgGJZM",
+    authDomain: "online-shop-projectjs.firebaseapp.com",
+    projectId: "online-shop-projectjs",
+    storageBucket: "online-shop-projectjs.firebasestorage.app",
+    messagingSenderId: "326894623124",
+    appId: "1:326894623124:web:30924e27a81f6358559781"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const provider = new GoogleAuthProvider();
+
+function signupWithGoogle(){
+    signInWithPopup(auth, provider)
+    .then((result) => {
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        const user = result.user;
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        const email = error.customData.email;
+        const credential = GoogleAuthProvider.credentialFromError(error);
+    })
+}
+
+const upGoogleButton = document.getElementById('signup_google');
+
+upGoogleButton.addEventListener('click', async(event) => {
+    event.preventDefault();
+    signupWithGoogle();
+})
+
+function createUser(email, password, auth) {
+    createUserWithEmailAndPassword (auth, email, password)
+    .then((userCredential) => {
+        const user = userCredential.user;
+        alert ('Регистрация прошла успешно')
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        alert ('Ошибка регистрации')
+    });
+}
+
+
+const signupForm = document.getElementById("signupForm");
+signupForm.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    const email = document.getElementById('signup_email').value;
+    const password = document.getElementById('signup_password').value;
+    const auth = getAuth();
+    createUser(email, password, auth);
+});
+
+document.querySelector(".header_signup_button").addEventListener("click", function() {
+    window.location.href = "signup_page.html";
+});
